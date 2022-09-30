d7e7d1a900196e7775c5b815eba8b338c97695b9
Anastasiyu <asy.safiulina@mail.ru>
2022-10-01 02:07:08 +0300

    Первый коммит

---
+++ src/main/java/com/example/newbook/NewBookApplication.java
@@ -0,0 +1,14 @@
+package com.example.newbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewBookApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/newbook/NewBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
